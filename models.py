6a647adc5afaec32e599a286a3ca88ba9ee6c854
essaon <carlionminon@gmail.com>
2024-02-12 00:25:12 +0300

    Fixed some bugs

--- models.py
+++ models.py
@@ -4,19 +4,15 @@ import telebot
 
 class SQLconnect():
     def __init__(self):
-        self.connect = sqlite3.connect('db.sqlite')
+        self.connect = sqlite3.connect('db.sqlite', check_same_thread=False)
         self.cursor = self.connect.cursor()
         self.cursor.execute('CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, quantity INTEGER)')
         self.cursor.execute('CREATE TABLE IF NOT EXISTS taken (id TEXT)')
         
     def AddUser(self, id):
-        self.connect = sqlite3.connect('db.sqlite')
-        self.cursor = self.connect.cursor()
         self.cursor.execute("INSERT INTO taken (id) VALUES (?)", (id,))
         self.connect.commit()
     def CreateButtons(self,):
-        self.connect = sqlite3.connect('db.sqlite')
-        self.cursor = self.connect.cursor()
         self.cursor.execute("SELECT name FROM items")
         names = [row[0] for row in self.cursor.fetchall()]
         markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
@@ -26,15 +22,11 @@ class SQLconnect():
 
         return markup
     def ListOfItems(self,):
-        self.connect = sqlite3.connect('db.sqlite')
-        self.cursor = self.connect.cursor()
         self.cursor.execute("SELECT name, description, quantity FROM items")
         answer = f"<pre>{tabulate(self.cursor.fetchall(), headers=['NAME', 'DISCR', 'QUANT'], tablefmt='orgtbl', colalign=('center',))}</pre>"
         return answer
 
     def ListOfTaken(self,):
-        self.connect = sqlite3.connect('db.sqlite')
-        self.cursor = self.connect.cursor()
         result = ''
         self.cursor.execute("PRAGMA table_info(taken)")
         columns = [column[1] for column in self.cursor.fetchall()]
@@ -55,16 +47,12 @@ class SQLconnect():
             return "Все предметы на месте!"
         
     def ListOfTakenByName(self, name):
-        self.connect = sqlite3.connect('db.sqlite')
-        self.cursor = self.connect.cursor()
         self.cursor.execute(f"SELECT id, {name.upper()} FROM taken")
         data = self.cursor.fetchall()
         answer = f"<pre>{tabulate(data, headers = ['TAGS', f'{name.upper()}'],  tablefmt='orgtbl', colalign=('center',))}</pre>"
         return answer
 
     def ListOfTakenByTag(self, name):
-        self.connect = sqlite3.connect('db.sqlite')
-        self.cursor = self.connect.cursor()
         self.cursor.execute("SELECT * FROM taken WHERE id = ?", (name,))
         data = self.cursor.fetchall()
         self.cursor.execute("SELECT name FROM items")
@@ -80,22 +68,16 @@ class SQLconnect():
         return answer
 
     def EditQuantity(self, name, quantity):
-        self.connect = sqlite3.connect('db.sqlite')
-        self.cursor = self.connect.cursor()
         self.cursor.execute("UPDATE items SET quantity = ? WHERE name = ?", (quantity, name.upper()))
         self.connect.commit()
 
     def DeleteItem(self, name):
-        self.connect = sqlite3.connect('db.sqlite')
-        self.cursor = self.connect.cursor()
         self.cursor.execute("DELETE FROM items WHERE name =?", (name.upper(),))
         self.cursor.execute(f"ALTER TABLE taken DROP COLUMN {name.upper()}")
         self.connect.commit()
         return "Успешно"
     
     def CreateItem(self, name, descr, quantity):
-        self.connect = sqlite3.connect('db.sqlite')
-        self.cursor = self.connect.cursor()
         if self.cursor.execute("SELECT name FROM items WHERE name = ?", (name.upper(),)).fetchall():
                 answer = "Такой предмет уже есть"
         else:
@@ -107,19 +89,15 @@ class SQLconnect():
         return answer
 
     def TakeItemDetail(self, name):
-        self.connect = sqlite3.connect('db.sqlite')
-        self.cursor = self.connect.cursor()
         self.cursor.execute("SELECT quantity FROM items WHERE name = ?", (name.upper(),))
         item = self.cursor.fetchone()
         if item:
-            answer = f"Кол-во: {item}\nСколько хотите взять?"
+            answer = f"Кол-во: {item[0]}\nСколько хотите взять?"
         else:
             answer = "Такого предмета нет"
         return answer
 
     def TakeItem(self, name, quantity, id):
-        self.connect = sqlite3.connect('db.sqlite')
-        self.cursor = self.connect.cursor()
         data = self.cursor.execute("SELECT quantity FROM items WHERE name=?", (name.upper(),)).fetchone()
         if int(quantity) > data[0]:
             return "Попытка взять больше, чем имеется"
@@ -133,24 +111,23 @@ class SQLconnect():
         return "Успешно"
     
     def ReturnItemDetail(self, id):
-        self.connect = sqlite3.connect('db.sqlite')
-        self.cursor = self.connect.cursor()
         answer = ""
         for i in self.cursor.execute("SELECT name FROM items").fetchall():
-            answer += f"{i[0]} - {str(self.cursor.execute(f"SELECT {i[0]} FROM taken WHERE id = ?", (id)).fetchall())}"
+            answer += f"{i[0]} - {str(self.cursor.execute(f"SELECT {i[0]} FROM taken WHERE id = ?", (id,)).fetchall())[2:-3:1]}\n"
         return answer
 
-    def ReturnItem(self, name, quantity, id):
-        self.connect = sqlite3.connect('db.sqlite')
-        self.cursor = self.connect.cursor()
-        data = self.cursor.execute(f"SELECT {name.upper()} FROM taken WHERE id = ?", (id)).fetchone()
-        if int(quantity) > data[0]:
-            return "Попытка вернуть больше, чем имеется"
-        self.crsor.execute(f"UPDATE taken SET {name.upper()} = ? WHERE id = ?", (data[0] - int(quantity), id))
-        self.connect.commit()
+    def ReturnItems(self, name, quantity, id):
+
+            data = self.cursor.execute(f"SELECT {name.upper()} FROM taken WHERE id = ?", (id,)).fetchone()
+            if (int(quantity) > data[0]):
+                    pass
+            else:
+                self.cursor.execute(f"UPDATE taken SET {name.upper()} = ? WHERE id = ?", (data[0] - int(quantity), id))
+                self.connect.commit()
+                
+                data = self.cursor.execute("SELECT quantity FROM items WHERE name = ?", (name.upper(),)).fetchone()
+                self.cursor.execute("UPDATE items SET quantity =? WHERE name =?", (data[0] + int(quantity), name.upper()))
+                self.connect.commit()
+
         
-        data = self.cursor.execute("SELECT quantity FROM items WHERE name = ?", (name.upper(),)).fetchone()
-        self.cursor.execute("UPDATE items SET quantity =? WHERE name =?", (data[0] + int(quantity), name.upper()))
-        self.connect.commit()
-        return "Успешно"
     
